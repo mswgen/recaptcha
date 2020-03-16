996986cbed2aaccfbfbbc044163b057adbae041b
mswgen <54498184+mswgen@users.noreply.github.com>
2020-03-16 18:28:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var yes = false;
+      var onloadCallback = function() {
+        grecaptcha.render('html_element', {
+          'sitekey' : '6LfdkOEUAAAAAHZQhQujPAnVqMQp177OZBO2vc3M',
+          'callback': function(){
+              yes = true;
+          }
+        });
+      };
+var yes = function(){
+    if(yes){
+        location.href = 'https://discordapp.com';
+    }
+}
